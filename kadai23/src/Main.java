0d5149844cd1901301697b571243166911b5b53c
kaomi <minami4349@gmail.com>
2019-03-08 14:53:26 +0900

    課題準備

--- kadai23/src/Main.java
+++ kadai23/src/Main.java
@@ -6,39 +6,63 @@ public class Main {
 		// TODO 自動生成されたメソッド・スタブ
 		/*
 		 * ■■Java課題（２３）■■　難易度★★★★
-20個の整数値を入力させた後、昇順に並べ替えをして空白で一行表示する。
-並べ替えは隣接交換法でプログラミングをすること。
-その後、探索したい整数値を一つ入力させ、一致したかしなかったかを表示する。
-一致した場合は、小さい数から何番目かを表示させる。
-また、探索は線形探索ではなく、
-二分探索でプログラミングすること。
- */
+		20個の整数値を入力させた後、昇順に並べ替えをして空白で一行表示する。
+		並べ替えは隣接交換法でプログラミングをすること。
+		その後、探索したい整数値を一つ入力させ、一致したかしなかったかを表示する。
+		一致した場合は、小さい数から何番目かを表示させる。
+		また、探索は線形探索ではなく、二分探索でプログラミングすること。
+		*/
 		Scanner scanner = new Scanner(System.in);
 
-
-
-
-		int a =  10;
+		int lenght = 8;
 		int t = 0;
-		int[] score = new int[a];
 		int low = 0;
-		int hi = score.length-1;
 		int mid;
-		int k;
-
-		mid = (low + hi)/2;
 
 
+		int[] score = new int[lenght];
+		int hi = score.length-1;
 
-		for (int i = 0; i < a; i++) {
+		for (int i = 0; i < lenght; i++) {
 
 			System.out.println("整数値を入力");
 			int num = new java.util.Scanner(System.in).nextInt();
-			 score[i] = num;
+			score[i] = num;
 
 		}
+
+		for (int value : score) {
+			System.out.print(value + "　");
+		}
+
+		System.out.println("");
+//隣接交換法
+		for (int j = score.length; j > 0; j--) {
+			for (int i = 0; i < score.length-1; i++) {
+				if (score[i] > score[i + 1]) {
+					t = score[i + 1];
+					score[i + 1] = score[i];
+					score[i] = t;
+				}
+			}
+		}
+
+
+		for (int value : score) {
+			System.out.print(value + "　");
+
+		}
+		System.out.println("");
 		System.out.println("探索値を入力");
-		 k = new java.util.Scanner(System.in).nextInt();
+		int k = new java.util.Scanner(System.in).nextInt();
+
+
+
+
+
+		System.out.println("");
+
+
 
 		while (low <= hi) {
 			mid = (low + hi)/2;
@@ -59,9 +83,7 @@ public class Main {
 
 		}
 
-		for(int value:score) {
-			System.out.print(value + "　");
-		}
+
 		if(t < 0) {
 			System.out.println("一致するものはありません");
 		}else {
@@ -70,6 +92,8 @@ public class Main {
 
 
 
+		System.out.println("");
+		System.out.println("end");
 
 	}
 
