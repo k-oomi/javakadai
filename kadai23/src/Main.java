0ed42f06c60ab5e750ced6a851b5f4221a331424
kaomi <minami4349@gmail.com>
2019-03-08 14:44:01 +0900

    課題23コピー

---
+++ kadai23/src/Main.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		/*
+		 * ■■Java課題（２３）■■　難易度★★★★
+20個の整数値を入力させた後、昇順に並べ替えをして空白で一行表示する。
+並べ替えは隣接交換法でプログラミングをすること。
+その後、探索したい整数値を一つ入力させ、一致したかしなかったかを表示する。
+一致した場合は、小さい数から何番目かを表示させる。
+また、探索は線形探索ではなく、
+二分探索でプログラミングすること。
+ */
+		Scanner scanner = new Scanner(System.in);
+
+
+
+
+		int a =  10;
+		int t = 0;
+		int[] score = new int[a];
+		int low = 0;
+		int hi = score.length-1;
+		int mid;
+		int k;
+
+		mid = (low + hi)/2;
+
+
+
+		for (int i = 0; i < a; i++) {
+
+			System.out.println("整数値を入力");
+			int num = new java.util.Scanner(System.in).nextInt();
+			 score[i] = num;
+
+		}
+		System.out.println("探索値を入力");
+		 k = new java.util.Scanner(System.in).nextInt();
+
+		while (low <= hi) {
+			mid = (low + hi)/2;
+
+			if(score[mid] == k) {
+				t = mid;
+				break;
+			}
+			if (score[mid] > k){
+				hi = mid-1;
+
+			}else {
+				low = mid+1;
+			}
+			if(score[mid] != k) {
+				t = -1;
+			}
+
+		}
+
+		for(int value:score) {
+			System.out.print(value + "　");
+		}
+		if(t < 0) {
+			System.out.println("一致するものはありません");
+		}else {
+			System.out.println(t+1 + "番目の数字と一致");
+		}
+
+
+
+
+	}
+
+}
